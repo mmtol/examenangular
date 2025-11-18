f5e6c81a2b025bad0cbe9d5b96bb361930f61d34
mmtol <mmigueltolosana@gmail.com>
2025-11-18 12:40:15 +0100

    perfil

--- src/components/perfil.component/perfil.component.ts
+++ src/components/perfil.component/perfil.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { OnInit } from '@angular/core';
+import ServiceCubos from '../../services/service.cubos';
+import Perfil from '../../models/Perfil';
 
-@Component({
+@Component
+({
   selector: 'app-perfil',
   standalone: false,
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css',
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit
+{
+  public perfil!:Perfil;
 
+  constructor(private _service:ServiceCubos,
+              private _router:Router){}
+
+  ngOnInit(): void 
+  {
+      let token = localStorage.getItem('token');
+      if (!token)
+      {
+        this._router.navigate(["/login"]);
+      }
+      else
+      {
+        this._service.getPerfil(token).then(response =>
+        {
+            this.perfil = response;
+        })
+      }
+  }
 }
