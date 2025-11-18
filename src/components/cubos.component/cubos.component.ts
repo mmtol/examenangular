7f871f41381ed8029b28f5111c72a10393c28b85
mmtol <mmigueltolosana@gmail.com>
2025-11-18 11:43:33 +0100

    cubos

--- src/components/cubos.component/cubos.component.ts
+++ src/components/cubos.component/cubos.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import Cubo from '../../models/Cubo';
+import ServiceCubos from '../../services/service.cubos';
 
-@Component({
+@Component
+({
   selector: 'app-cubos',
   standalone: false,
   templateUrl: './cubos.component.html',
   styleUrl: './cubos.component.css',
 })
-export class CubosComponent {
+export class CubosComponent implements OnInit
+{
+  public cubos!:Array<Cubo>;
 
+  constructor(private _service:ServiceCubos){}
+
+  ngOnInit(): void 
+  {
+    this._service.getCubos().then(response =>
+    {
+      this.cubos = response;
+    }) 
+  }
 }
