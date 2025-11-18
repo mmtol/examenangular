bcf1efc6a2eee06ec7b7415b6f64e04508fedb29
mmtol <mmigueltolosana@gmail.com>
2025-11-18 11:54:47 +0100

    medio login

--- src/components/login.component/login.component.ts
+++ src/components/login.component/login.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import ServiceCubos from '../../services/service.cubos';
+import Usuario from '../../models/Usuario';
 
-@Component({
+@Component
+({
   selector: 'app-login',
   standalone: false,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent 
+{
+  public usuario!:Usuario;
 
+  @ViewChild("cajaEmail") cajaEmail:ElementRef;
+  @ViewChild("cajaPasswd") cajaPasswd:ElementRef;
+
+  constructor(private _service:ServiceCubos,
+              private _router:Router)
+  {
+    this.cajaEmail = new ElementRef("");
+    this.cajaPasswd = new ElementRef("");
+  }
+
+  iniciarSesion()
+  {
+    let email = this.cajaEmail.nativeElement.value;
+    let passwd = this.cajaPasswd.nativeElement.value;
+
+    this.usuario = new Usuario(email, passwd);
+
+    this._service.iniciarSesion(this.usuario).then(response =>
+    {
+      localStorage.setItem('token', response.response);
+    })
+  }
 }
