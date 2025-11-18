86b74bedc91bfec6268d037d7534f8d05bbc55bd
mmtol <mmigueltolosana@gmail.com>
2025-11-18 11:48:49 +0100

    cubos por marca

--- src/components/cubos-marcas.component/cubos-marcas.component.ts
+++ src/components/cubos-marcas.component/cubos-marcas.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Params } from '@angular/router';
+import Cubo from '../../models/Cubo';
+import ServiceCubos from '../../services/service.cubos';
 
-@Component({
+@Component
+({
   selector: 'app-cubos-marcas',
   standalone: false,
   templateUrl: './cubos-marcas.component.html',
   styleUrl: './cubos-marcas.component.css',
 })
-export class CubosMarcasComponent {
+export class CubosMarcasComponent implements OnInit
+{
+  public cubos!:Array<Cubo>;
+  public marca!:string;
 
+  constructor(private _service:ServiceCubos,
+              private _activeRoute:ActivatedRoute){}
+
+  ngOnInit(): void 
+  {
+    this._activeRoute.params.subscribe((parametros:Params)=>
+    {
+      if (parametros["marca"] != null)
+      {
+        this.marca = parametros["marca"];
+        
+        this._service.getCubosMarca(this.marca).then(response =>
+        {
+          this.cubos = response;
+        })  
+      }
+    })
+
+  }
 }
